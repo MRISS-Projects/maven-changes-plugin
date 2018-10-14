073001c80edd0ad1f84df15c08cbe37efab4cbd8
Marcelo Riss <marcelo.riss@hp.com>
2018-10-13 23:13:11 -0300

    removing mocked test version

--- src/test/java/org/apache/maven/plugin/github/GitHubTextListMojoTestCase.java
+++ src/test/java/org/apache/maven/plugin/github/GitHubTextListMojoTestCase.java
@@ -20,12 +20,9 @@ package org.apache.maven.plugin.github;
  */
 
 import java.io.File;
-import java.util.Properties;
 
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
-import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.configuration.PlexusConfiguration;
-import org.mockito.Mockito;
 
 /**
  * 
@@ -72,26 +69,6 @@ public class GitHubTextListMojoTestCase extends AbstractMojoTestCase
         catch ( Exception e )
         {
             e.printStackTrace();
-            // Try with mockito
-            try
-            {
-                GitHubTextListMojo mojo = Mockito.mock( GitHubTextListMojo.class );
-                assertNotNull( mojo );
-                MavenProject pm = Mockito.mock( MavenProject.class );
-                Properties p = new Properties();
-                p.setProperty( "current.release.issues", "MOCKED CURRENT RELEASE ISSUES" );
-                p.setProperty( "older.releases.issues", "MOCKED OLDER RELEASES ISSUES" );
-                Mockito.when( pm.getProperties() ).thenReturn( p );
-                Mockito.when( mojo.getMavenProject() ).thenReturn( pm );
-                mojo.execute();
-                System.out.println( mojo.getMavenProject().getProperties().get( "current.release.issues" ) );
-                System.out.println( mojo.getMavenProject().getProperties().get( "older.releases.issues" ) );
-            }
-            catch ( Exception e2 )
-            {
-                e.printStackTrace();
-                fail( e.getMessage() );
-            }
         }
     }
 
